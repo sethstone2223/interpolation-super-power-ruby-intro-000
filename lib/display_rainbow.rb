8209e16645a8a169cf734e611ad57d89b30a398c
Seth Stone <srs15e@acu.edu>
2017-12-20 23:58:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])  
+def display_rainbow(['', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+R: = red
+O: = orange
+Y: = yellow
+G: = green
+B: = blue
+I: = indigo
+V: = violet
