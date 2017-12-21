3ca589425dda41e765ce192b5443f43e122ecc5d
Seth Stone <srs15e@acu.edu>
2017-12-21 00:52:43 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,3 @@
 def display_rainbow (colors)
   puts colors "#{letters[0]}, #{letters[1]}, #{letters[2]}, #{letters[3]}, #{letters[4]}, #{letters[5]}, #{letters[6]}"
 end
-letters = ["R","O", "Y", "G", "B", "I", "V"]
-colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
