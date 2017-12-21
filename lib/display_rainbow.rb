435065dc988957dfb3f4485e8fbb731ae7b34db9
Seth Stone <srs15e@acu.edu>
2017-12-21 00:22:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,5 @@
 letters = ["R: red","O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
+colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
 def display_rainbow letters
+  puts colors.join(", ")
 end
