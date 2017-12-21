8242244a7dfc09c2d80601dd1b74c53436a962e1
Seth Stone <srs15e@acu.edu>
2017-12-21 00:02:47 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,8 @@
-puts R = [red]
-puts O = [orange]
-puts Y = [yellow]
-puts G = [green]
-puts B = [blue]
-puts I = [indigo]
-puts V = [violet]
+R = [red]
+O = [orange]
+G [green]
+B = [blue]
+I = [indigo]
+V = [violet]
 def display_rainbow(['R', 'O', 'Y', 'G', 'B', 'I', 'V'])
 end
