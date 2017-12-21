6eacace387c3d7dcae1e73999059bd1650dc287f
Seth Stone <srs15e@acu.edu>
2017-12-21 00:53:43 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow (colors)
-  puts colors "#{letters[0]}, #{letters[1]}, #{letters[2]}, #{letters[3]}, #{letters[4]}, #{letters[5]}, #{letters[6]}"
+  puts letters "#{letters[0]}, #{letters[1]}, #{letters[2]}, #{letters[3]}, #{letters[4]}, #{letters[5]}, #{letters[6]}"
 end
